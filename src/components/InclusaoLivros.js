bd45a6f311478963db42f410df738197ff7dd4ca
dacasfilipe <filiperdacas@gmail.com>
2023-06-21 20:38:17 -0300

    inicio codigo incluso livros

---
+++ src/components/InclusaoLivros.js
@@ -0,0 +1,12 @@
+//Componente para incluir livros no banco de dados
+//declaração da função do componente IncluirLivros
+const InclusaoLivros = () => {
+    return ( //aqui é o que vai ser exibido em tela
+        <div className="container">
+            
+
+        </div>
+    )
+}
+
+export default InclusaoLivros;
