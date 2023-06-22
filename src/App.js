f7d75ddfde9e7e48461e15deaed12198dd02fc80
dacasfilipe <filiperdacas@gmail.com>
2023-06-21 21:20:13 -0300

    alterado app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import MenuSuperior from './components/MenuSuperior';
+import InclusaoLivros from './components/InclusaoLivros';
 
 const App = () => {
   return(  //tudo que vai aqui no return é o que aparece na aplicação
     <>
       <MenuSuperior/>
+      <InclusaoLivros/>
     </>
   )
 }
 //Serão criados 2 componentes para essa aplicação
 //MenuSuperior.js
 //InclusaoLivros.js
-
-
 export default App;
