09451d3b03be1205699180bb228bb1a1abbbc6f1
dacasfilipe <filiperdacas@gmail.com>
2023-06-28 20:18:00 -0300

    inserido novos componentes

---
+++ src/components/ResumoLivros.js
@@ -0,0 +1,9 @@
+//criando a função
+const ResumoLivros = () => {
+    //retornando uma visualização
+    return(
+        <h2>Resumo Livros</h2>
+    )
+};
+//exportando a função
+export default ResumoLivros;
