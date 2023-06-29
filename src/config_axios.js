74a8dfa6ae4f718c6b4ad673d5c99d16cd28b447
dacasfilipe <filiperdacas@gmail.com>
2023-06-28 22:02:41 -0300

    atualizado config-axios

--- src/config_axios.js
+++ src/config_axios.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 //baseURL é a porta que está rodando o backend
 export const api = axios.create({
   baseURL: 'http://localhost:3001'
+  //GET localhost:3001/livros
 });
 
 
